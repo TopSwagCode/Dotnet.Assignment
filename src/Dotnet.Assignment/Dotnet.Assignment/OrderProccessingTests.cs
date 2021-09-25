edc92a20699e21b7f80e39824c3fb550c01c61bd
topswagcode <josh@topswagcode.com>
2021-09-25 07:39:01 +0200

    Adding abstraction for handlers. Abstract class and a interface.

--- src/Dotnet.Assignment/Dotnet.Assignment/OrderProccessingTests.cs
+++ src/Dotnet.Assignment/Dotnet.Assignment/OrderProccessingTests.cs
@@ -24,6 +24,29 @@ namespace Dotnet.Assignment
 
     }
 
+    // Idea for Abstraction
+    // Have a abstract class with a Next handler.
+    // Basic interface for setting Next, like AspNetCore uses AddUseRouting etc.
+    // TODO: Implement interface for 2 existing Handlers and see how it would work.
+    public abstract class Handler<T> : IHandler<T> where T : class
+    {
+        private IHandler<T> Next { get; set; }
+        public virtual void Handle(T request)
+        {
+            Next?.Handle(request);
+        }
+        public IHandler<T> SetNext(IHandler<T> next)
+        {
+            Next = next;
+            return Next;
+        }
+    }
+    public interface IHandler<T> where T : class
+    {
+        IHandler<T> SetNext(IHandler<T> next);
+        void Handle(T request);
+    }
+
     public class OrderProcessor
     {
         public OrderProcessor()
